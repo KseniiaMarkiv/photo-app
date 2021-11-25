adbcefbad677ab1b98169d47448890b88ca107e8
KseniiaMarkiv <ksenya.markiv@gmail.com>
2021-11-25 20:05:14 +0200

    Setup email confirmation and style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'photo-app-ubuntu.herokuapp.com', :protocol => 'https' }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -29,7 +31,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
